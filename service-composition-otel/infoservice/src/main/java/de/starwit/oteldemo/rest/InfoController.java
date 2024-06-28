00a02353abbd958fea147ab419cff0c6740a68a1
ztarbug <kronprinzfriedrich@gmx.de>
2024-06-28 20:50:24 +0200

    work on otel example services

--- service-composition-otel/infoservice/src/main/java/de/starwit/oteldemo/rest/InfoController.java
+++ service-composition-otel/infoservice/src/main/java/de/starwit/oteldemo/rest/InfoController.java
@@ -4,9 +4,7 @@ import java.util.Collections;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import de.starwit.oteldemo.entities.InfoEntity;
+import jakarta.annotation.PostConstruct;
 
 @RestController
 @RequestMapping(path = "/api/info")
@@ -22,6 +21,15 @@ public class InfoController {
 
     @Value("${pdf.service.url}")
     String pdfServiceUri;
+
+    @Value("${logo.service.url}")
+    String logoServiceUri;    
+
+    @PostConstruct
+    public void outputStatus() {
+        System.out.println("PDF Service URL " + pdfServiceUri);
+        System.out.println("Logo Service URL " + logoServiceUri);
+    }
     
     @GetMapping("/")
     public ResponseEntity<InfoEntity> test() {
@@ -40,7 +48,12 @@ public class InfoController {
         headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
         headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
 
-        final String response = restTemplate.getForObject(pdfServiceUri, String.class);
+        System.out.println("Calling PDF service");
+        String response = restTemplate.getForObject(pdfServiceUri, String.class);
+        System.out.println(response);
+
+        System.out.println("Calling Logo service");
+        response = restTemplate.getForObject(logoServiceUri, String.class);
         System.out.println(response);
 
         InfoEntity i = new InfoEntity();
