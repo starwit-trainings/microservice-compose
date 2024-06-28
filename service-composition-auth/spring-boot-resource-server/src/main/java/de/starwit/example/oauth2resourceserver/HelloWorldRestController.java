0c5b4b357cba29aa5e1fa24ffdd5ec3dfef86a8b
flonix8 <flstanek@googlemail.com>
2024-06-28 11:05:57 +0200

    Add second resource server backend; Separate the frontend into nginx container; Make frontend a bit nicer

--- service-composition-auth/spring-boot-resource-server/src/main/java/de/starwit/example/oauth2resourceserver/HelloWorldRestController.java
+++ service-composition-auth/spring-boot-resource-server/src/main/java/de/starwit/example/oauth2resourceserver/HelloWorldRestController.java
@@ -2,6 +2,7 @@ package de.starwit.example.oauth2resourceserver;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.server.resource.authentication.AbstractOAuth2TokenAuthenticationToken;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -12,9 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "*")
 public class HelloWorldRestController {
 
+    @Value("${identification}")
+    private String identification;
+    
     @GetMapping("/public-endpoint")
     public String publicEndpoint() {
-        return "Nothing interesting to see here, please continue.";
+        return "<p>" + identification + "</p>Nothing interesting to see here, please continue.";
     }
 
     @GetMapping("/read-endpoint")
@@ -22,17 +26,17 @@ public class HelloWorldRestController {
         String username = (String) authToken.getTokenAttributes().get("preferred_username");
         List<String> authorities = authToken.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList();
 
-        return String.format("Hi %s<br><br>P.S.: You have the following roles %s", username, authorities);
+        return String.format("<p>" + identification + "</p>Hi %s<br><br>P.S.: You have the following roles %s", username, authorities);
     }
 
     @GetMapping("/write-endpoint")
     public String writeEndpoint() {
-        return "You have write privileges!";
+        return "<p>" + identification + "</p>You have write privileges!";
     }
 
     @GetMapping("/admin-endpoint")
     public String adminEndpoint() {
-        return "With great power comes great responsibility &#x1F4A5";
+        return "<p>" + identification + "</p>With great power comes great responsibility &#x1F4A5";
     }
 
 }
--- service-composition-auth/spring-boot-resource-server/src/main/java/de/starwit/example/oauth2resourceserver/SecurityConfig.java
+++ service-composition-auth/spring-boot-resource-server/src/main/java/de/starwit/example/oauth2resourceserver/SecurityConfig.java
@@ -6,7 +6,6 @@ import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.GrantedAuthority;
@@ -14,10 +13,6 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.servlet.function.RequestPredicates;
-import org.springframework.web.servlet.function.RouterFunction;
-import org.springframework.web.servlet.function.RouterFunctions;
-import org.springframework.web.servlet.function.ServerResponse;
 
 @Configuration
 public class SecurityConfig {
@@ -26,8 +21,6 @@ public class SecurityConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests(authorize -> authorize
-                .requestMatchers("/").permitAll()
-                .requestMatchers("/index.html").permitAll()
                 .requestMatchers("/public-endpoint").permitAll()
                 .requestMatchers("/read-endpoint").hasAnyAuthority("read", "write", "admin")
                 .requestMatchers("/write-endpoint").hasAnyAuthority("write", "admin")
@@ -49,12 +42,6 @@ public class SecurityConfig {
         JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
         jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
         return jwtAuthenticationConverter;
-    }
-
-    @Bean
-    RouterFunction<ServerResponse> indexRouter() {
-        ClassPathResource index = new ClassPathResource("static/index.html");
-        return RouterFunctions.route().resource(RequestPredicates.path("/"), index).build();
     }
 
 }
